238a0de18be32dc90747c4e160b64b679a237cce
thebestlad <95973487+thebestlad@users.noreply.github.com>
2022-07-20 12:17:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+var a=1;
+var b=1;
+var c=1;
+var d=1;
+var e=1;
+var f=1;
+function clicker1(){
+    if(a==1){
+        a=0
+        document.querySelector('.block1').style.height="150px";
+    }
+    else{
+        a=1;
+        document.querySelector(".block1").style.height="0px";
+        document.querySelector('#img1').script="https://static.thenounproject.com/png/1479017-200.png";
+    }
+}
+function clicker2(){
+    if(b==1){
+        b=0
+        document.querySelector(".block2").style.height="70px";
+    }
+    else{
+        b=1
+        document.querySelector(".block2").style.height="0px";
+    }
+}
+function clicker3(){
+    if(c==1){
+c=0
+document.querySelector(".block3").style.height="200px";
+    }
+    else{
+c=1
+document.querySelector(".block3").style.height="0px";
+    }
+}
+function clicker4(){
+    if(d==1){
+d=0
+document.querySelector('.block4').style.height="70px";
+    }
+    else{
+d=1
+document.querySelector('.block4').style.height="0px";
+    }
+}
+function clicker5(){
+    if (e==1){
+        e=0;
+        document.querySelector('.block5').style.height="70px";
+    }
+    else{
+        e=1
+        document.querySelector('.block5').style.height="0px";
+    }
+}
+function clicker6(){
+    if(f==1){
+        f=0;
+        document.querySelector('.block6').style.height="150px";
+    }
+    else{
+        f=1
+        document.querySelector('.block6').style.height="0px";
+    }
+}
